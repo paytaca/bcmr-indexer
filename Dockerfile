5d7b0038ceb7a3ca12a9e453acc4f2f2b001a548
Jet Albano <skyjeth1371120@gmail.com>
2023-05-14 10:41:40 +0800

    Added deployment setup

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,21 @@
 FROM nikolaik/python-nodejs:python3.7-nodejs15-slim
 
-COPY ./requirements.txt requirements.txt
-RUN apt-key adv --refresh-keys --keyserver keyserver.ubuntu.com \
-    && apt-get update \
-    && apt-get install -y --no-install-recommends g++ \
-    gettext \
-    && pip install --upgrade pip \
-    && pip install --no-cache-dir --use-deprecated=legacy-resolver -r requirements.txt \
-    && apt-get purge -y --auto-remove gcc
-
-WORKDIR /app
-COPY . /app
-
-RUN npm install
-
-RUN wget -O /usr/local/bin/wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/8ed92e8cab83cfed76ff012ed4a36cef74b28096/wait-for-it.sh && \
+RUN apt-key adv --refresh-keys --keyserver keyserver.ubuntu.com
+RUN apt-get update -y
+RUN apt-get -y install build-essential sudo postgresql libpq-dev postgresql-client curl \
+    postgresql-client-common libncurses5-dev libjpeg-dev zlib1g-dev git wget redis-server && \
+    wget -O /usr/local/bin/wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/8ed92e8cab83cfed76ff012ed4a36cef74b28096/wait-for-it.sh && \
     chmod +x /usr/local/bin/wait-for-it.sh
 
-EXPOSE 8000
-ENTRYPOINT [ "/bin/sh", "entrypoint.sh" ]
-CMD [ "supervisord", "-c", "/app/supervisord.conf", "--nodaemon" ]
+RUN pip install --upgrade pip
+COPY ./requirements.txt requirements.txt
+# RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . /code
+WORKDIR /code
+
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+
+ENTRYPOINT [ "sh", "entrypoint.sh" ]
---
+++ fabfile.py
@@ -0,0 +1,108 @@
+from patchwork.transfers import rsync
+from fabric import task
+from fabric.connection import Connection
+from dotenv import dotenv_values
+
+
+config = dotenv_values(".env")
+hosts = [ 'root@' + config['SERVER_IP'] ]
+project = "paytaca-bcmr"
+
+
+@task
+def chipnet(ctx):
+    server = config['SERVER']
+    ctx.config.network = 'chipnet'
+    user = server.split('@')[0]
+    host = server.split('@')[1]
+    ctx.config.run.env['conn'] = Connection(
+        host,
+        user=user,
+        connect_kwargs = { 'key_filename': config['SERVER_SSH_KEY'] }
+    )
+
+
+@task
+def uname(ctx):
+    conn = ctx.config.run.env['conn']
+    conn.run('uname -a')
+
+
+@task
+def sync(ctx):
+    conn = ctx.config.run.env['conn']
+    rsync(
+        conn,
+        '.',
+        f'/home/ubuntu/{project}',
+        exclude=[
+            '.venv',
+            '.git',
+            '/static',
+            '.DS_Store',
+            '.env',
+            '__pycache__',
+            '*.pyc',
+            '*.log',
+            '*.pid',
+            '/postgres-data',
+            '/redis-data',
+        ]
+    )
+    with conn.cd(f'/home/ubuntu/{project}'):
+        conn.run(f'sudo cp compose/.env_{ctx.config.network} .env')
+
+
+@task
+def build(ctx):
+    conn = ctx.config.run.env['conn']
+    with conn.cd(f'/home/ubuntu/{project}'):
+        conn.run(f'sudo docker-compose -f compose/{ctx.config.network}.yml build')
+
+
+@task
+def up(ctx):
+    conn = ctx.config.run.env['conn']
+    with conn.cd(f'/home/ubuntu/{project}'):
+        conn.run(f'sudo docker-compose -f compose/{ctx.config.network}.yml up -d')
+
+
+@task
+def down(ctx):
+    conn = ctx.config.run.env['conn']
+    with conn.cd(f'/home/ubuntu/{project}'):
+        conn.run(f'sudo docker-compose -f compose/{ctx.config.network}.yml down')
+
+
+@task
+def deploy(ctx):
+    sync(ctx)
+    build(ctx)
+    down(ctx)
+    up(ctx)
+
+
+@task
+def nginx(ctx):
+    sync(ctx)
+    conn = ctx.config.run.env['conn']
+    with conn.cd(f'/home/ubuntu/{project}/compose'):
+        nginx_conf = f"/etc/nginx/sites-available/{project}"
+        nginx_slink = f"/etc/nginx/sites-enabled/{project}"
+
+        try:
+            conn.run(f'sudo rm {nginx_conf}')
+            conn.run(f'sudo rm {nginx_slink}')
+        except:
+            pass
+
+        conn.run(f'sudo cat nginx.conf > {nginx_conf}')
+        conn.run(f'sudo ln -s {nginx_conf} {nginx_slink}')
+        conn.run('sudo service nginx restart')
+
+
+@task
+def logs(ctx):
+    conn = ctx.config.run.env['conn']
+    with conn.cd(f'/home/ubuntu/{project}'):
+        conn.run(f'sudo docker-compose -f compose/{ctx.config.network}.yml logs -f web')
