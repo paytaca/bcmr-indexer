9814673e440a22d5d8889d20cd74be25466a2a87
ubuntu <rongrammer@hotmail.com>
2023-10-26 21:01:38 +0800

    updated mempool listener

--- bcmr_main/management/commands/mempool_listener.py
+++ bcmr_main/management/commands/mempool_listener.py
@@ -7,11 +7,13 @@
 from django.core.management.base import BaseCommand
 
 from bcmr_main.bchn import *
+from bcmr_main.op_return import process_op_return
 from bcmr_main.tasks import process_tx
 
 import logging
 import binascii
 import zmq
+from bitcoinrpc.authproxy import AuthServiceProxy
 
 
 LOGGER = logging.getLogger(__name__)
@@ -25,8 +27,9 @@ class ZMQHandler():
 
         self.zmqContext = zmq.Context()
         self.zmqSubSocket = self.zmqContext.socket(zmq.SUB)
-        self.zmqSubSocket.setsockopt_string(zmq.SUBSCRIBE, "hashtx")
+        self.zmqSubSocket.setsockopt_string(zmq.SUBSCRIBE, "rawtx")
         self.zmqSubSocket.connect(self.url)
+        self.rpc_connection = AuthServiceProxy(settings.BCHN_NODE)
 
     def start(self):
         try:
@@ -34,23 +37,29 @@ class ZMQHandler():
                 msg = self.zmqSubSocket.recv_multipart()
                 topic = msg[0].decode()
                 body = msg[1]
-
-                if topic == "hashtx":
-                    tx_hash = binascii.hexlify(body).decode()
-                    tx = self.BCHN._get_raw_transaction(tx_hash)
-                    inputs = tx['vin']
-
-                    if 'coinbase' in inputs[0].keys():
-                        continue
-                    
-                    process_tx.delay(tx_hash, source='mempool')
+                if topic == "rawtx":
+                    decoded = self.rpc_connection.decoderawtransaction(body.hex())
+                    outputs = decoded.get('vout')
+                    for output in outputs:
+                        if output['type'] == 'nulldata' and output['scriptPubKey'].get('asm').startswith('OP_RETURN'):
+                            index = output['n']
+                            txid  = decoded['txid']
+                            asm   = output['scriptPubKey'].get('asm')
+                            process_op_return(
+                                txid,
+                                index,
+                                asm,
+                                #publisher?,
+                                #time?
+                            )
+                            # asm_arr = output['scriptPubKey'].get('asm').split(' ') 
+                            # if len(asm_arr) >= 4:  
+                            #     uris_hex = output['scriptPubKey'].get('asm').split(' ')[4]
+                            #     uris = bytes.fromhex(uris_hex).decode('utf-8')
+                            #     uris = uris.split(';')
+                            #     if len(uris)
         except KeyboardInterrupt:
-            zmqContext.destroy()
+            self.zmqContext.destroy()
 
-
-class Command(BaseCommand):
-    help = "Start mempool tracker using ZMQ"
-
-    def handle(self, *args, **options):
-        daemon = ZMQHandler()
-        daemon.start()
+z = ZMQHandler()
+z.start()
