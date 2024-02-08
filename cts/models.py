508c545dcef1f37e1d73c448cb93a16a94dff7c9
ubuntu <rongrammer@hotmail.com>
2024-02-08 12:44:06 +0800

    commented model for now

--- cts/models.py
+++ cts/models.py
@@ -1,16 +1,16 @@
-from django.db import models
-from bcmr.models import TimeStampedModel
-from bcmr_main.models import Registry
+# from django.db import models
+# from bcmr.models import TimeStampedModel
+# from bcmr_main.models import Registry
 
 
-class NftParsingInformation:
+# class NftParsingInformation:
 
-  category = models.CharField(max_length=64, unique=True)
-  bytecode = models.CharField(default=None, max_length=255, null=True, blank=True)
-  registry = models.ForeignKey(Registry, on_delete=models.CASCADE)
+#   category = models.CharField(max_length=64, unique=True)
+#   bytecode = models.CharField(default=None, max_length=255, null=True, blank=True)
+#   registry = models.ForeignKey(Registry, on_delete=models.CASCADE)
 
-  @property
-  def nft_collection_type(self):
-    if self.bytecode:
-      return 'ParsableNftCollection'
-    return 'SequentialNftCollection'
+#   @property
+#   def nft_collection_type(self):
+#     if self.bytecode:
+#       return 'ParsableNftCollection'
+#     return 'SequentialNftCollection'
--- cts/serializers.py
+++ cts/serializers.py
@@ -1,15 +1,15 @@
-from rest_framework import serializers
-from .models import *
+# from rest_framework import serializers
+# from .models import *
 
 
-class TokenSerializer(serializers.ModelSerializer):
+# class TokenSerializer(serializers.ModelSerializer):
 
-    nft_collection_type = serializers.Field()
+#     nft_collection_type = serializers.Field()
 
-    class Meta:
-        model = NftParsingInformation
-        fields = (
-            'bytecode',
-            'fields',
-            'nft_collection_type'
-        )
+#     class Meta:
+#         model = NftParsingInformation
+#         fields = (
+#             'bytecode',
+#             'fields',
+#             'nft_collection_type'
+#         )
