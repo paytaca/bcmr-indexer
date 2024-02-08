75ccf45f50e64c9826fba2f839d6c544be3dfb26
ubuntu <rongrammer@hotmail.com>
2024-02-08 14:44:12 +0800

    support for get nft by commitment

--- cts/views/NftType.py
+++ cts/views/NftType.py
@@ -10,12 +10,15 @@ class NftType(APIView):
     """
     def get(self, request, *args, **kwargs):
         category = kwargs.get('category', '')
+        commitment = kwargs.get('commitment', '')
         limit = request.query_params.get('limit')
         offset = request.query_params.get('offset')
-        
         registry = Registry.find_registry_by_token_category(category)
         if registry:
             r = Registry.objects.get(id=registry['registry_id'])
             if r:
-                return JsonResponse(r.get_nft_types(category, int(limit or 2), int(offset or 0)), safe=False)
+                if commitment:
+                    return JsonResponse(r.get_nft_type(category, commitment), safe=False)
+                else:
+                    return JsonResponse(r.get_nft_types(category, int(limit or 2), int(offset or 0)), safe=False)
         return JsonResponse(data=None, safe=False)
