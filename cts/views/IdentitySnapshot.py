027949dbb86b6592f6f9c56dd329fc56fba3cc1f
ubuntu <rongrammer@hotmail.com>
2024-02-08 14:43:53 +0800

    only include token in identitysnapshot if include_token query is specified

--- cts/views/IdentitySnapshot.py
+++ cts/views/IdentitySnapshot.py
@@ -8,9 +8,13 @@ class IdentitySnapshot(APIView):
 
     def get(self, request, *args, **kwargs):
         category = kwargs.get('category', '')
+        include_token = request.query_params.get('include_token', '')
         registry = Registry.find_registry_by_token_category(category)
         if registry:
             r = Registry.objects.get(id=registry['registry_id'])
             if r:
-                return JsonResponse(r.get_identity_snapshot(category) | {})
+                if include_token and include_token.lower() == 'true':
+                    return JsonResponse(r.get_identity_snapshot(category) | {})
+                return JsonResponse(r.get_identity_snapshot_basic(category) | {})
+                
         return JsonResponse(data=None, safe=False)
