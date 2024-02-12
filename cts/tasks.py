394072b41733d957ffae00c0e77a5901ede9ef5f
ubuntu <rongrammer@hotmail.com>
2024-02-12 16:07:49 +0800

    Cache IdentitySnapshot

--- cts/tasks.py
+++ cts/tasks.py
@@ -1,11 +1,17 @@
+import redis
+import json
+import datetime
+from decouple import config
 from celery import shared_task
+from bcmr_main.models import Registry
 
 @shared_task()
-def set_category_nfttype_cache(category, nfttype):
-  # cache the nft type
-  pass 
+def update_identity_snapshot_cache(category):
+  client = redis.Redis(host=config('REDIS_HOST', 'redis'), port=config('REDIS_PORT', 6379))
+  registry = Registry.find_registry_id(category)
+  if registry:
+      r = Registry.objects.get(id=registry['registry_id'])
+      if r:
+            identity_snapshot = r.get_identity_snapshot_basic(category)
+            client.set(f'{category}_identity-snapshot',json.dumps({**identity_snapshot, 'time': datetime.datetime.utcnow()}), ex=(60 * 30))
 
-@shared_task()
-def set_category_nfttypes_cache(category, nfttype, page=1):
-  # cache the nfttypes page
-  pass 
--- cts/views/registry/IdentitySnapshot.py
+++ cts/views/registry/IdentitySnapshot.py
@@ -1,21 +1,40 @@
+import redis
+import json
+
+from decouple import config
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from django.http import JsonResponse
 from bcmr_main.models import Registry
+from cts.tasks import update_identity_snapshot_cache
 
 class IdentitySnapshot(APIView):
     allowed_methods = ['GET']
 
     def get(self, request, *args, **kwargs):
+
         category = kwargs.get('category', '')
-        include_token = request.query_params.get('include_token', '')
+        include_token_nfts = True if request.query_params.get('include_token_nfts', '').lower() == 'true' else False
+        client = redis.Redis(host=config('REDIS_HOST', 'redis'), port=config('REDIS_PORT', 6379))
+        identity_snapshot = client.get(f'{category}_identity-snapshot')
+
+        if identity_snapshot and not include_token_nfts:
+            identity_snapshot = json.loads(identity_snapshot)
+            # Update cache every time it's touched
+            # Only cache basic IdentitySnapshot (without nfts)
+            update_identity_snapshot_cache.delay(category)
+            return JsonResponse(identity_snapshot, safe=False)
+        
         registry = Registry.find_registry_id(category)
         if registry:
             r = Registry.objects.get(id=registry['registry_id'])
             if r:
-                if include_token and include_token.lower() == 'true':
-                    return JsonResponse(r.get_identity_snapshot(category) | {})
-                return JsonResponse(r.get_identity_snapshot_basic(category) | {})
+                if include_token_nfts:
+                    identity_snapshot = r.get_identity_snapshot(category)
+                else:
+                    identity_snapshot = r.get_identity_snapshot_basic(category)
+                    client.set(f'{category}_identity-snapshot',json.dumps(identity_snapshot), ex=(60 * 30))
+                return JsonResponse(identity_snapshot, safe=False)
                 
         return JsonResponse(data=None, safe=False)
