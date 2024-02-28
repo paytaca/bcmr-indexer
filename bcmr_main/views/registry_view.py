09ba3f301612e5ebdb039ec50a54a11e7c4f1f3c
joemarct <joemar.ct@gmail.com>
2024-02-29 02:15:04 +0800

    Fixed category without registry returning error 500 instead of 404, added registry caching

--- bcmr_main/views/registry_view.py
+++ bcmr_main/views/registry_view.py
@@ -1,6 +1,11 @@
 from rest_framework.views import APIView
 from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
 from bcmr_main.models import Registry
+from decouple import config
+import redis
+import json
 
 class RegistryView(APIView):
     
@@ -10,6 +15,17 @@ class RegistryView(APIView):
         category = kwargs.get('category', '')
         if not category:
             return JsonResponse(data=None, safe=False)
-
-        registry = Registry.objects.filter(contents__identities__has_key=category).latest('id')
-        return JsonResponse(data=registry.contents, safe=False)
+        
+        client = redis.Redis(host=config('REDIS_HOST', 'redis'), port=config('REDIS_PORT', 6379))
+        cache_key = f'registry:token:{category}'
+        cached_response = client.get(cache_key)
+        if cached_response:
+            response = json.loads(cached_response)
+        else:
+            try:
+                registry = Registry.objects.filter(contents__identities__has_key=category).latest('id')
+                response = registry.contents
+                client.set(cache_key, json.dumps(response), ex=(60 * 60 * 24))
+            except Registry.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse(response, safe=False)
