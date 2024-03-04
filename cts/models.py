e367db6fea2524d260f82fda394ce9cac70906e4
ubuntu <rongrammer@hotmail.com>
2024-03-04 18:02:12 +0800

    fixed tokencategory metadata

--- cts/models.py
+++ cts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from bcmr_main.models import Token
 from bcmr_main.models import Registry
 from decouple import config
-from .tasks import update_nftmetadata_cache
+from .tasks import update_nftmetadata_cache, update_tokencategorymetadata_cache
 
 class CashToken(Token):
   
@@ -37,32 +37,27 @@ class CashToken(Token):
   def token_category(self):
 
     client = redis.Redis(host=config('REDIS_HOST', 'redis'), port=config('REDIS_PORT', 6379))
-    metadata = client.get(f'{self.category}_token-category')
-
+    cache_key = f'tokencategorymetadata:{self.category}'
+    metadata = client.get(cache_key)
+    
     if metadata:
       metadata = json.loads(metadata)
+      update_tokencategorymetadata_cache.delay(self.category, self.commitment, cache_key)
       return metadata
 
-    registry_id = client.get(f'{self.category}_registry-id')
+    r = Registry.objects.filter(contents__identities__has_key=self.category)
 
-    if registry_id:
-      registry_id = int(registry_id.decode())
-    else:
-      registry = Registry.find_registry_id(self.category)
-      if registry and registry.get('registry_id'):
-        registry_id = registry.get('registry_id')
-        client.set(f'{self.category}_registry-id', registry_id, ex=(60 * 30))
+    if r.exists():
+      r = r.latest('id')
+      if r:
+        try:
+          metadata = r.get_token_category_basic(category=self.category)
+          if metadata:
+            client.set(cache_key,json.dumps(metadata), ex=(60 * 30))
+        except Exception as e:
+          pass
 
-    if registry_id:
-      registry_instance = Registry.objects.get(id=registry_id)
-      metadata = None 
-      try:
-        metadata = registry_instance.get_token_category_basic(category=self.category)
-        if metadata:
-          client.set(f'{self.category}_token-category',json.dumps(metadata), ex=(60 * 30))
-      except Exception as e:
-        pass
-      return metadata
+    return metadata
       
   class Meta:
     proxy = True
