62cf7ede78fa570878e07734236e4d3dac59bd08
ubuntu <rongrammer@hotmail.com>
2025-04-23 23:18:37 +0800

    async update cache on hit

--- bcmr_main/views/token_view.py
+++ bcmr_main/views/token_view.py
@@ -5,48 +5,13 @@ from decouple import config
 from rest_framework.views import APIView
 from django.http import JsonResponse
 from bcmr_main.models import Registry, Token
+from bcmr_main.tasks import update_registry_and_nft_cache
 from rest_framework.views import APIView
 from django.http import JsonResponse
 import dateutil.parser
 from operator import itemgetter
 from dateutil.parser import parse as parse_datetime
-
-
-def transform_to_paytaca_expected_format(identity_snapshot, nft_type_key, is_nft):
-    nft_type_key_exists = False
-    if nft_type_key:
-        if identity_snapshot.get('token') and identity_snapshot['token'].get('nfts'):
-            nfts = identity_snapshot['token'].pop('nfts')
-            if nft_type_key == 'empty' or nft_type_key == 'none':
-                nft_type_key = ''
-            nft_type_details = (nfts.get('parse') or {}).get('types' or {}).get(nft_type_key)
-            if nft_type_details: 
-                nft_type_key_exists = True
-                identity_snapshot['type_metadata'] = nfts['parse']['types'][nft_type_key]
-
-                # NOTE: This is a temporary fix for NFTs that do not have `image` field under `uris`
-                # The `image` field is needed by Paytaca to display NFTs propery in the wallet
-                type_uris = identity_snapshot['type_metadata']['uris']
-                if 'asset' in type_uris.keys()  and 'image' not in type_uris.keys():
-                    asset_uri = identity_snapshot['type_metadata']['uris']['asset']
-                    asset_uri_ext = asset_uri.split('.')[-1].lower()
-                    if asset_uri_ext in ['jpg', 'png', 'gif', 'svg']:
-                        identity_snapshot['type_metadata']['uris']['image'] = asset_uri
-
-                # For some collections that only specified an icon but not an image
-                if 'icon' in type_uris.keys()  and 'image' not in type_uris.keys():
-                    identity_snapshot['type_metadata']['uris']['image'] = identity_snapshot['type_metadata']['uris']['icon']
-            
-    if identity_snapshot.get('token') and identity_snapshot['token'].get('nfts'):
-        identity_snapshot['token'].pop('nfts')
-
-    if identity_snapshot.get('_meta'):
-        identity_snapshot.pop('_meta')
-
-    if identity_snapshot:
-        identity_snapshot['is_nft'] = is_nft
-    
-    return (identity_snapshot, nft_type_key_exists)
+from bcmr_main.utils import transform_to_paytaca_expected_format
 
 class TokenView(APIView):
 
@@ -74,6 +39,7 @@ class TokenView(APIView):
 
         if cached_response:
             response = json.loads(cached_response)
+            update_registry_and_nft_cache.delay(category, nft_type_key)
         else:
             registry = Registry.objects.filter(contents__identities__has_key=category, publisher__identities__contains=[category])
             if registry.exists():
