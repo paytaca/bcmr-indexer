499e01906f6086cf96a4c385f2972421dc95ccb4
joemarct <joemar.ct@gmail.com>
2025-06-24 23:08:00 +0800

    Added parsing of non-standard BCMR registry files from old tokens

--- bcmr_main/views/token_view.py
+++ bcmr_main/views/token_view.py
@@ -80,25 +80,43 @@ class TokenView(APIView):
                 r = registry.latest('publisher_id')
                 if r:
                     identity_snapshots = r.contents['identities'][category]
-                    snapshot_keys = identity_snapshots.keys()
-                    snapshots = []
-                    for snapshot_key in snapshot_keys:
-                        try:
-                            snapshots.append([snapshot_key, parse_datetime(snapshot_key)])
-                        except dateutil.parser._parser.ParserError:
-                            pass
-                    snapshots.sort(key=itemgetter(1))
-                    latest_key, history_date = snapshots[-1]
-                    identity_snapshot = identity_snapshots[latest_key]
-                    if identity_snapshot:
-                        response, nft_type_key_exists = transform_to_paytaca_expected_format(identity_snapshot, nft_type_key, is_nft)
-                        if nft_type_key:
-                            if nft_type_key_exists:
-                                client.set(f'metadata:token:{category}:{nft_type_key}', json.dumps(response), ex=(60 * 60 * 24))
+                    
+                    # Handle non-standard BCMR format where identity_snapshots is a list
+                    if isinstance(identity_snapshots, list):
+                        # If it's a list, take the first (and presumably only) item
+                        if identity_snapshots:
+                            identity_snapshot = identity_snapshots[0]
+                            if identity_snapshot:
+                                response, nft_type_key_exists = transform_to_paytaca_expected_format(identity_snapshot, nft_type_key, is_nft)
+                                if nft_type_key:
+                                    if nft_type_key_exists:
+                                        client.set(f'metadata:token:{category}:{nft_type_key}', json.dumps(response), ex=(60 * 60 * 24))
+                                    else:
+                                        # Saving, the default token metadata of non existing key, but expire early
+                                        client.set(f'metadata:token:{category}:{nft_type_key}', json.dumps(response), ex=(60 * 15))
+                                else:
+                                    client.set(f'metadata:token:{category}', json.dumps(response), ex=(60 * 60 * 24))
+                    else:
+                        # Standard BCMR format - dictionary with timestamp keys
+                        snapshot_keys = identity_snapshots.keys()
+                        snapshots = []
+                        for snapshot_key in snapshot_keys:
+                            try:
+                                snapshots.append([snapshot_key, parse_datetime(snapshot_key)])
+                            except dateutil.parser._parser.ParserError:
+                                pass
+                        snapshots.sort(key=itemgetter(1))
+                        latest_key, history_date = snapshots[-1]
+                        identity_snapshot = identity_snapshots[latest_key]
+                        if identity_snapshot:
+                            response, nft_type_key_exists = transform_to_paytaca_expected_format(identity_snapshot, nft_type_key, is_nft)
+                            if nft_type_key:
+                                if nft_type_key_exists:
+                                    client.set(f'metadata:token:{category}:{nft_type_key}', json.dumps(response), ex=(60 * 60 * 24))
+                                else:
+                                    # Saving, the default token metadata of non existing key, but expire early
+                                    client.set(f'metadata:token:{category}:{nft_type_key}', json.dumps(response), ex=(60 * 15))
                             else:
-                                # Saving, the default token metadata of non existing key, but expire early
-                                client.set(f'metadata:token:{category}:{nft_type_key}', json.dumps(response), ex=(60 * 15))
-                        else:
-                            client.set(f'metadata:token:{category}', json.dumps(response), ex=(60 * 60 * 24)) 
+                                client.set(f'metadata:token:{category}', json.dumps(response), ex=(60 * 60 * 24))
 
         return JsonResponse(response, safe=False)
